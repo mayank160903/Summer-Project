64dfa30f73406aefee8c79c84622d6d9cc95b0f9
Harshit <97081728+H-C-21@users.noreply.github.com>
2023-04-15 18:18:43 +0530

    Add files via upload
    Models Folder for MongoDB

---
+++ models/course.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const productSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'teacher', required: true},
+  category:{type: String, required: true},
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+
+});
+
+module.exports = mongoose.model('Product', productSchema);
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+  
+  fullname: {type: String,required: true},
+  username: { type: String, required: true},
+  email: { type: String, required: true },
+  phone: { type: Number, required: true},
+  password: { type: String, required: true },
+});
+
+
+module.exports =  mongoose.model('Users', userSchema);
